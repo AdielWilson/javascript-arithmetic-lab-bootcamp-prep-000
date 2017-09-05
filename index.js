582c80a6f9732f862e9a403723683a70f70d706d
Adiel Wilson <adielwilson@gmail.com>
2017-09-05 01:06:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-
-add ( a , b ) { return a + b; };
-subtract ( a , b ) { return a - b; };
-multiply ( a , b ) { return a * b; };
-divide ( a , b ) { return a / b; };
-inc ( n ) { return ++n; };
-dec ( n ) { return --n; };
-makeInt ( n ) { return parseInt (n,10); };
-preserveDecimal ( n ) { return parseFloat(n); };
+function add ( a , b ) { return a + b; };
+function subtract ( a , b ) { return a - b; };
+function multiply ( a , b ) { return a * b; };
+function divide ( a , b ) { return a / b; };
+function inc ( n ) { return ++n; };
+function dec ( n ) { return --n; };
+function makeInt ( n ) { return parseInt (n,10); };
+function preserveDecimal ( n ) { return parseFloat(n); };

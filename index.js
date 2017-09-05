3ca7f1cbc5ea3162d804a7f660ce1cdacdf04c8c
Adiel Wilson <adielwilson@gmail.com>
2017-09-05 00:35:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-divide(a,b) { return a/b; };
-inc(n){ return ++n; };
-dec(n){ return --n; };
-makeInt(n) { return parseInt (n,10); };
-preserveDecimal(n){ return parseFloat(n); };
+add ( a , b ) { return a + b; };
+subtract ( a , b ) { return a - b; };
+multiply ( a , b ) { return a * b; };
+divide ( a , b ) { return a / b; };
+inc ( n ) { return ++n; };
+dec ( n ) { return --n; };
+makeInt ( n ) { return parseInt (n,10); };
+preserveDecimal ( n ) { return parseFloat(n); };
